afeb42079ca709773c0b262d00014026cd24db6e
chrisliu <761486612@qq.com>
2017-04-24 22:51:49 +0800

    删除联系方式部分内容

--- src/contact.js
+++ src/contact.js
@@ -19,21 +19,21 @@ Vue.component('v-contact', {
 			title: "联系我／Contact",
 			info: "扫描或点击图片二维码",
 			message: {
-				"新浪微博": {
-					name: "有一个姑娘在coding",
-					pic: "./img/weibo.jpg",
-					href: "http://weibo.com/u/5750254341?refer_flag=1001030101_&is_hot=1"
-				},
-				"微信公众号":{
-					name: "有一个姑娘在coding",
-					pic: "./img/weixin.jpg",
-					href: "http://mp.weixin.qq.com/mp/homepage?__biz=MjM5MDk5MzE2Mw==&hid=1&sn=c156f12e6c46f78445fd1dbc957d44a3#wechat_redirect"
-				},
-				"github": {
-					name: "sunweiling",
-					pic: "./img/github.jpg",
-					href: "https://github.com/sunweiling"
-				}
+				// "新浪微博": {
+				// 	name: "有一个姑娘在coding",
+				// 	pic: "./img/weibo.jpg",
+				// 	href: "http://weibo.com/u/5750254341?refer_flag=1001030101_&is_hot=1"
+				// },
+				// "微信公众号":{
+				// 	name: "有一个姑娘在coding",
+				// 	pic: "./img/weixin.jpg",
+				// 	href: "http://mp.weixin.qq.com/mp/homepage?__biz=MjM5MDk5MzE2Mw==&hid=1&sn=c156f12e6c46f78445fd1dbc957d44a3#wechat_redirect"
+				// },
+				// "github": {
+				// 	name: "sunweiling",
+				// 	pic: "./img/github.jpg",
+				// 	href: "https://github.com/sunweiling"
+				// }
 			},
 			mail:"317764217@qq.com"
 
